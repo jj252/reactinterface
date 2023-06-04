b79a496fb89468746ff7268661ee35eb23d2bf59
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-06-04 16:02:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { BsFillAlarmFill } from "react-icons/bs";
 
 function App() {
+  const dishes = [
+    'Black Bean Soup',
+    'Macaroni and Cheese',
+    'Salmon and Potatoes',
+    'Pizza'
+  ];
+
+  const Main = (props) => {
+    return(
+    <section>
+          <ul>
+            {props.dishes.map((dish) =>(
+              <li>{dish}</li>
+            ))}
+          </ul>
+        </section>
+    )
+  }
+
   return (
     <div className="App">
+
       <header className="App-header">
+        <BsFillAlarmFill />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main dishes={dishes}/>
         <a
           className="App-link"
           href="https://reactjs.org"
